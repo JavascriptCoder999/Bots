7f38bb5bf06cdcd50aafc45812f5bd43413fb273
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-05-23 06:37:56 +0100

    Update Fernando.sh

--- Fernando.sh
+++ Fernando.sh
@@ -1,70 +1,106 @@
 #!/bin/bash
 echo "Fernando_coolguy11 entered the chat"
 sleep 1s
+echo ""
 echo "Hi my name is Fernando"
 sleep 2s
+echo ""
 echo "They call me Nando"
 sleep 2s
+echo ""
 echo "Even though I don't like Nandos"
 sleep 2.5s
+echo ""
 echo "Five years ago I snotted on my sister and she said oh my god bro"
 sleep 4s
+echo ""
 echo "I am currently twenty years old and I live in a condo"
 sleep 2.5s
+echo ""
 echo "I drive around in a Benz with my friends"
 sleep 2s
+echo ""
 echo "Actually I have no friends that was just a joke so..."
 sleep 1.5s
+echo ""
 echo "Yeah"
 sleep 1s
+echo ""
+echo ""
 echo "My grandmother is really old"
 sleep 1.5s
+echo ""
 echo "She even likes to sew"
 sleep 1.5s
+echo ""
 echo "Once I drove too fast the cops came and got my car towed"
 sleep 2.5s
+echo ""
 echo "I just spent fifteen hours on Fortnite trying to figure out if this sus dude was a friend or foe"
 sleep 5s
+echo ""
 echo "I still don't really know"
 sleep 1.5s
+echo ""
 echo "My friend came round my house and said a sussy joke"
 sleep 2s
+echo ""
 echo "But my mum heard and five minutes later she had the cops on the phone"
 sleep 3s
+echo ""
 echo "And then in unison we both said oh hell no"
 sleep 2s
+echo ""
 echo "I really think the sussiest place to go is Rome"
 sleep 1.5s
+echo ""
 echo "I once got a cop warning to stop vaping because it was a no-smoke zone"
 sleep 3s
+echo ""
 echo "I once bought a book called DIY how to make a gravestone"
 sleep 2.5s
+echo ""
 echo "I used to get bullied because I have no cheekbones"
 sleep 2s
+echo ""
 echo "Now I can't talk too well because I spilled water on my microphone"
 sleep 3s
+echo ""
 echo "I'm not too good at science but I know the word dihydroxyacetone"
 sleep 4s
+echo ""
 echo "Don't know what it means though"
 sleep 1.5s
+echo ""
 echo "Can somebody please tell me why my mum keeps saying watch your tone"
 sleep 2.5s
+echo ""
 echo "Oh my god lets go I just found some Minecraft redstone"
 sleep 2.5s
+echo ""
 echo "Had it tucked away in a chest near some sandstone"
 sleep 2.5s
+echo ""
 echo "Oh no"
 sleep 1s
+echo ""
 echo "My mum wants the lawn mowed"
 sleep 1.5s
+echo ""
 echo "And she wants me to buy a new lawn gnome"
 sleep 2s
+echo ""
 echo "So I gotta go"
 sleep 1s
+echo ""
 echo "Don't forget to check out my Linktree though (@milkjug21)!"
 sleep 2.5s
+echo ""
 echo "Fernando_coolguy11 left the chat"
 sleep 4s
+echo ""
+echo ""
+echo ""
 echo "End credits: Fernando was coded by JavascriptCoder999 and please contact him if you would like to use this also Fernando's full name is Fernando Towel Stinky Gelatin Halo Taco Bell Burper and Fernando comes from the family of bots who go by the name Polin, which you can find at his Polin Github repository."
 fim -a Fernando.png
 exit
