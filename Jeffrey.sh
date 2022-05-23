8e26b9df857a150183e5a4251c36f626b6980520
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-05-23 07:37:22 +0100

    Create Jeffrey.sh

---
+++ Jeffrey.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+echo "Jeffrey_rchamp11 entered the chat"
+sleep 0.5s
+echo ""
+echo "Hi losers it's your main man JEFF"
+sleep 1.5s
+echo ""
+echo "I teach you to roast better and I only charge 30 JBucks (£30) per lesson"
+sleep 2.5s
+echo "Okay!"
+sleep 1s
+echo ""
+echo "Here are some Yo Mama roasts:"
+sleep 2s
+tput reset
+echo "Before I tell you these, you need to know the Yo Mama categories."
+sleep 3s
+echo "These are:"
+sleep 1s
+tput reset
+echo "Fat"
+sleep 1s
+echo "Ugly"
+sleep 1s
+echo "Stupid"
+sleep 1s
+echo "Hairy"
+sleep 1s
+echo "Tall"
+sleep 1s
+echo "Short"
+sleep 1s
+echo "Karen"
+sleep 1s
+echo "Skinny"
+sleep 1s
+echo "Poor"
+sleep 1s
+echo "Old"
+sleep 1s
+echo "Depressing."
+echo ""
+echo ""
+sleep 1s
+echo "There are a few more but these are the most offensive, and the more offensive, the better."
+sleep 4s
+tput reset
+echo "Here we go..."
+sleep 1.5s
+echo ""
+echo "Yo mama so old, when she walked out the museum the alarm went off."
+sleep 2s
+echo ""
+echo "Yo mama so poor, she had to win Squid Game five times just to break even."
+sleep 2s
+echo ""
+echo "Yo mama so depressing, blues singers come to visit her when they have writers block."
+sleep 4s
+echo ""
+echo "Yo mama so skinny, she hula hoops in a cheerio."
+sleep 2.5s
+echo ""
+echo "Yo mama so karen, when I asked her to go to hell, she did. And she first thing she did there was
+
+ ask for the manager."
+sleep 6s
+echo ""
+echo "Yo mama so short, she plays football with atoms."
+sleep 3s
+echo ""
+echo "Yo mama so tall, she can't play Minecraft because she's hgher than the build limit."
+sleep 5.5s
+echo ""
+echo "Yo mama so hairy, when she walked into the Star Wars movie, people thought she was Chewbacca."
+sleep 6s
+echo ""
+echo "Yo mama so stupid, she shoved a battery up her butt, and thought she was a power ranger."
+sleep 6s
+echo ""
+echo "Yo mama so ugly, she's the reason monsters hide under the bed."
+sleep 4s
+echo ""
+echo "Yo mama so fat, she was the iceberg that sunk the titanic."
+sleep 3s
+echo ""
+echo ""
+echo "FAT ROAST TIME!!!"
+sleep 2s
+tput reset
+echo "Yo mama so fat, when she stepped on the scales, it read my phone number!!!"
+sleep 3.5s
+echo ""
+echo "Yo mama so fat, when she stepped on the scales, Buzz Lightyear poped out and said to infinity and
+
+ beyond!!!"
+sleep 7s
+echo ""
+echo "Yo mama so fat, when she went outside in a green bikini, people thought she was Godzilla!!!"
+sleep 6s
+echo ""
+echo "Yo mama so fat, her passport picture had to be taken by a helicopter!!!"
+sleep 3s
+echo ""
+echo "Yo mama so fat, her blood type is gravy!!!"
+sleep 1.5s
+echo ""
+echo "Yo mama so fat, the crocodile on her Lacoste polo shirt is real!!!"
+sleep 2.5s
+echo ""
+echo "STUPID ROAST TIME!!!"
+sleep 1s
+tput reset
+echo "Yo mama so stupid, she shoved two quarters in her ears and thought she was listening to 50 Cent!!!"
+sleep 7s
+echo ""
+echo "Yo mama so stupid, she bought tickets to Xbox Live!!!"
+sleep 2.5s
+echo ""
+echo "Yo mama so stupid, she drank battery acid thinking it was an energy drink!!!"
+sleep 3s
+echo "Yo mama so stupid, she tried to climb Mountain Dew!!!"
+sleep 2s
+echo ""
+echo ""
+echo "Yo mama so stupid, she tried to have an operation at Dr Pepper!!!"
+sleep 2.5s
+echo ""
+echo ""
+echo ""
+echo "OTHER ROAST TIME!!!"
+sleep 1s
+tput reset
+echo "Yo mama so fat, her belly button gets home fifteen minues before she does!!!"
+sleep 3s
+echo ""
+echo "Yo mama so ugly, when she threw a boomerang, it wouldn't come back!!!"
+sleep 3s
+echo ""
+echo "Yo mama so ugly, she made Hello Kitty say goodbye!!!"
+sleep 2s
+echo ""
+echo "Yo mama so ugly, she made One Direction go the other direction!!!"
+sleep 3s
+echo ""
+echo "Yo mama so stupid, when robbers broke into her house, she ran downstairs, dialed 911 on the
+
+ microwave, and couldn't find the call button!!!"
+sleep 8s
+echo ""
+echo "Yo mama so ugly, even Dora couldn't explore her!!!"
+sleep 2s
+echo ""
+echo "Yo mama so ugly, when Fix It Felix saw her, he said I can't fix this!!!"
+sleep 3.5s
+echo ""
+echo "Yo mama so fat, when she sat on Walmart she lowered the prices!!!"
+sleep 2s
+echo ""
+echo "Yo mama so old, when I told her to act her age, she frickin' died!!!"
+sleep 2.5s
+echo ""
+echo "Yo mama so old, she pre-ordered the bible!!!"
+sleep 2s
+echo ""
+echo "Yo mama so fat, when God said let there be light, she meant for her to move out the way"
+sleep 5s
+echo ""
+echo "BOOM YOU JUST GOT ROASTED!!!"
+sleep 1s
+echo ""
+echo "Hey I bet you were born on the motorway cause that's where most accidents happen."
+sleep 3s
+echo "Jeffrey_rchamp11 left the chat"
+sleep 4s
+echo "End credits: Jeffrey was coded by JavascriptCoder999 and please contact him if you would like to
+
+ use this also Jeffrey's full name is Jeffrey Jeff Rey Rhea Rye Bean Soy Chandelier and Jeffrey comes
+
+ from the family of bots who go by the name Polin, which you can find at his Polin Github repository."
+clear
+fim -a Jeffrey.png
+exit
+#roast battle with jeffrey - iconic
