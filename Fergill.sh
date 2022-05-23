54798487b1eb3c1a0db67b1186b39524917a1c1d
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-05-23 06:34:07 +0100

    Update Fergill.sh

--- Fergill.sh
+++ Fergill.sh
@@ -1,76 +1,123 @@
 #!/bin/bash
 echo "Fergill_bot11 entered the chat"
 sleep 0.5s
+echo ""
 echo "Hi"
 sleep 1s
+echo ""
 echo "It is none other than..."
 sleep 1s
+echo ""
 echo "Me"
 sleep 1s
+echo ""
 echo "Fergill"
 sleep 1s
+echo ""
 echo "Your favourite bot"
 sleep 1.5s
+echo ""
 echo "Ever"
 sleep 1s
+echo ""
 echo "(but to be honest people only ever need me if they are idiots)"
 sleep 1s
+echo ""
 echo "I am 20 years old"
 sleep 1s
+echo ""
 echo "And the purpose of my existence is to give you advice"
 sleep 1s
+echo ""
 echo "Here's my first tip:"
 sleep 1s
+echo ""
 echo "Never"
 sleep 2s
+echo ""
 echo "Poop"
 sleep 2s
+echo ""
 echo "Away"
 sleep 2s
+echo ""
 echo "From"
 sleep 3.14159265
+echo ""
 echo "Home."
 sleep 1s
+echo ""
 echo "You got that?"
 sleep 1s
+echo ""
 echo "Okay!"
 sleep 1s
+echo ""
 echo "Tip 2:"
 sleep 1s
+echo ""
 echo "Do"
 sleep 1s
+echo ""
 echo "Not"
 sleep 1s
+echo ""
 echo "Not"
 sleep 1s
+echo ""
 echo "Not"
 sleep 1s
+echo ""
 echo "Not"
 sleep 1s
+echo ""
 echo "Eat"
 sleep 1s
+echo ""
 echo "Lemon"
 sleep 1s
+echo ""
 echo "Curd."
 sleep 1s
+echo ""
 echo "Or you are absurd"
 sleep 1s
+echo ""
 echo "Okay good"
 sleep 1s
+echo ""
 echo "Last tip:"
 sleep 2s
-echo "01000110 01100101 01110010 01100111 01101001 01101100 01101100 00100000 01101001 01110011 00100000 01100110 01101111 01110010 00100000 01101001 01100100 01101001 01101111 01110100 01110011 00100000 01100001 01101110 01100100 00100000 01111001 01101111 01110101 00100000 01101000 01100001 01110110 01100101 00100000 01110101 01110011 01100101 01100100 00100000 01101001 01110100 00100000 01101000 01100001 01101000 01100001"
+echo ""
+echo "01000110 01100101 01110010 01100111 01101001 01101100 01101100 00100000 01101001 01110011 00100000
+
+ 01100110 01101111 01110010 00100000 01101001 01100100 01101001 01101111 01110100 01110011 00100000
+
+ 01100001 01101110 01100100 00100000 01111001 01101111 01110101 00100000 01101000 01100001 01110110
+
+ 01100101 00100000 01110101 01110011 01100101 01100100 00100000 01101001 01110100 00100000 01101000
+
+ 01100001 01101000 01100001"
 sleep 1s
+echo ""
 echo "Can't crack that binary??"
 sleep 1s
-echo "46 65 72 67 69 6C 6C 20 69 73 20 66 6F 72 20 69 64 69 6F 74 73 20 61 6E 64 20 79 6F 75 20 68 61 76 65 20 75 73 65 64 20 69 74 20 68 61 68 61"
+echo ""
+echo "46 65 72 67 69 6C 6C 20 69 73 20 66 6F 72 20 69 64 69 6F 74 73 20 61 6E 64 20 79 6F 75 20 68 61 76
+
+ 65 20 75 73 65 64 20 69 74 20 68 61 68 61"
 sleep 1s
+echo ""
 echo "Can't crack that hex??"
 sleep 1s
+echo ""
 echo "Just go away"
 sleep 1s
+echo ""
 echo "Fergill_bot11 left the chat"
 sleep 4s
+echo ""
+echo ""
 echo "End credits: Coded by @JavascriptCoder999, by the way, Fergill's full name is Fergill Kill Bill Pill Still Ill Quill Mill Till."
 fim -a Fergill.png
 exit
